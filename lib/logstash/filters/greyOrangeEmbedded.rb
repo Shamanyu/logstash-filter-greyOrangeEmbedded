d0bf7b8fd009d1401d532c3bdd747c93c6d6d9a5
gor <gor@graphite-01>
2015-12-23 15:28:49 +0530

    Modifications for generalization

--- lib/logstash/filters/greyOrangeEmbedded.rb
+++ lib/logstash/filters/greyOrangeEmbedded.rb
@@ -2,54 +2,34 @@
 require "logstash/filters/base"
 require "logstash/namespace"
 
-# This example filter will replace the contents of the default 
-# message field with whatever you specify in the configuration.
-#
-# It is only intended to be used as an example.
 class LogStash::Filters::GreyOrangeEmbedded < LogStash::Filters::Base
-
-  # Setting the config_name here is required. This is how you
-  # configure this filter from your Logstash config.
-  #
-  # filter {
-  #   example {
-  #     message => "My message..."
-  #   }
-  # }
-  #
   config_name "greyOrangeEmbedded"
+  config :keys, :validate => :string
   
-  # Replace the message with this value.
-  config :keys
-  
-
   public
   def register
-    # Add instance variables 
-  end # def register
+  end
 
   public
   def filter(event)
-    if @keys
-      # Replace the event message with our message as configured in the
-      # config file.
-     	event["keys"] = @keys
-	field_array = event["keys"].split("|")
-	$array_length=field_array.length
-	$counter=0
-	while $counter < $array_length do
-		custom_event=LogStash::Event.new()
-                field_value=field_array[$counter].split(":")
-                custom_event["unixTime"]=%{unixTime}
-                custom_event["severity"]=%{severity}
-                custom_event["attribute"]=field_value[0]
-                custom_event["metric"]=field_value[1]
-                $counter += 1
-                yield custom_event
-    	end
-     end
-     filtered_match(event)
-     #Cancel the main event
-     #event.cancel
-  end # def filter
-end # class LogStash::Filters::GreyOrangeEmbedded
+	if event["keys"].is_a?(String)
+        	field_array = event["keys"].split("|")
+		$array_length=field_array.length
+		$counter=0
+		while $counter < $array_length do
+			custom_event=LogStash::Event.new()
+        		field_value=field_array[$counter].split(":")
+			custom_event["host"]=event["tags[0]"]
+			custom_event["service"]="butler_embedded"
+			custom_event["unixTime"]=event["unixTime"]
+			custom_event["severity"]=event["severity"]
+        		custom_event["attribute"]=field_value[0]
+       	 		custom_event["metric"]=field_value[1]
+        		$counter += 1
+			filter_matched(custom_event)
+        		yield custom_event
+    		end
+	end
+     	event.cancel
+  end
+end
